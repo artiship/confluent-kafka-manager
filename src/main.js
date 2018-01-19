eeabba980bd3fd66caaa87e2061d841bde33811b
artiship <meilziner@gmail.com>
2018-01-19 23:02:48 +0800

    add spectre css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import 'spectre.css/dist/spectre.css'
+import 'spectre.css/dist/spectre-exp.css'
+import 'spectre.css/dist/spectre-icons.css'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
