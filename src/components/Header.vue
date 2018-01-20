5902c472292569ba2edd07c6e494c17c9f4663cf
artiship <meilziner@gmail.com>
2018-01-20 21:41:40 +0800

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <header class="navbar container">
+    <div class="navbar-section">
+      <a href="#navbar" class="navbar-brand mr-2">Confluent Kafka Manager</a>
+    </div>
+    <div class="navbar-section">
+      <a href="https://github.com/picturepan2/spectre" class="btn btn-link">GitHub</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
