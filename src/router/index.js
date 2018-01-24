52f545fcf06463d77bf6c38a7d386726d2032b1c
artiship <meilziner@gmail.com>
2018-01-24 22:19:25 +0800

    store history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '@/components/dashboard/Dashboard'
+import TopicList from '@/components/topic/TopicList'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'dashbaord',
       component: Dashboard
-    }
+    },
+    {
+      path: '/topic',
+      name: 'topicList',
+      component: TopicList
+    },
   ]
 })
